9dc7002ee5cd994c04f10457a642833ecc4b2d6e
hi <hi@LAPTOP-12E4CL3N>
2024-08-26 21:50:37 +0530

    commit

---
+++ src/firstPackage/FirstClass.java
@@ -0,0 +1,11 @@
+package firstPackage;
+
+public class FirstClass {
+public static void main(String[] args) {
+	System.out.println("vijay rangu");
+	 play();
+}
+public static void play() {
+	System.out.println("play");
+}
+}
